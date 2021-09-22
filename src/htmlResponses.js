de3e44eea905b060d0a9bb3174b01832f40efc7e
Long Hoang <heroprotunlong@gmail.com>
2021-09-22 13:58:26 -0400

    added joke-client

--- src/htmlResponses.js
+++ src/htmlResponses.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const errorPage = fs.readFileSync(`${__dirname}/../client/error.html`);
 const defaultCSS = fs.readFileSync(`${__dirname}/../css/default-styles.css`);
+const jokeClient = fs.readFileSync(`${__dirname}/../client/joke-client.html`);
 
 const get404Response = (request, response) => {
   response.writeHead(404, { 'Content-Type': 'text/html' });
@@ -15,7 +16,14 @@ const getCSSResponse = (request, response) => {
   response.end();
 };
 
+const getJokeClient = (request, response) => {
+  response.writeHead(404, { 'Content-Type': 'text/html' });
+  response.write(jokeClient);
+  response.end();
+};
+
 module.exports = {
   get404Response,
   getCSSResponse,
+  getJokeClient,
 };
